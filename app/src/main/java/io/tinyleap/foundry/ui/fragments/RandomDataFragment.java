1722ab91e56d3f177063b59ea8df85cdb9effd88
Elton Kent <ek@tinyleap.io>
2019-11-02 12:19:03 -0400

    ui changes

--- app/src/main/java/io/tinyleap/foundry/ui/fragments/RandomDataFragment.java
+++ app/src/main/java/io/tinyleap/foundry/ui/fragments/RandomDataFragment.java
@@ -8,6 +8,8 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import io.tinyleap.foundry.R;
 import io.tinyleap.foundry.containers.ComponentDetail;
+import io.tinyleap.util.random.RandomColor;
+import io.tinyleap.util.random.RandomString;
 
 public class RandomDataFragment extends ItemDetailFragment {
 
@@ -16,7 +18,21 @@ public class RandomDataFragment extends ItemDetailFragment {
     protected void addChildContent(ComponentDetail detail, LayoutInflater inflater, ViewGroup parent) {
         View rootView = inflater.inflate(R.layout.fragment_random_data, null, false);
         parent.addView(rootView);
-        TextView t1=rootView.findViewById(R.id.description);
-        t1.setText("This sis a texst");
+        View random=rootView.findViewById(R.id.randomString);
+        final TextView tv=rootView.findViewById(R.id.genString);
+        random.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText(RandomString.randomAlphanumeric(150));
+            }
+        });
+        random=rootView.findViewById(R.id.randomColor);
+        final View tvcolor=rootView.findViewById(R.id.genColor);
+        random.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvcolor.setBackgroundColor(RandomColor.getRandomColor());
+            }
+        });
     }
 }
